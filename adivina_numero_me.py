9b1467991a971cacf3d66b9f8125db9ff1fb50ba
leonard256 <squall_leonard128@hotmail.com>
2022-11-06 10:45:38 -0500

    Create adivina_numero_me.py

---
+++ adivina_numero_me.py
@@ -0,0 +1,95 @@
+import random
+
+def ahorcado(intento):
+    if intento == 6:
+        print(""" 
+                -------
+                |      |
+                |      o
+                |     \ /            
+                |      | 
+               / \    / \ 
+              /   \   
+                
+            0 Intentos faltantes _X_ _X_ _X_ _X_ _X_ _X_
+                    😥 PERDISTE 😥""")
+
+    elif intento == 5:
+        print(""" 
+                -------
+                |      |
+                |      o
+                |     \ /  
+                |      | 
+               / \    /  
+              /   \   
+                
+            1 Intento faltante _X_ _X_ _X_ _X_ _X_ __""")
+
+    elif intento == 4:
+        print(""" 
+                -------
+                |      |
+                |      o
+                |     \ / 
+                |      | 
+               / \      
+              /   \   
+                
+            2 Intentos faltantes _X_ _X_ _X_ _X_ __ __""")
+
+    elif intento == 3:
+        print(""" 
+                -------
+                |      |
+                |      o
+                |     \ /
+                |       
+               / \      
+              /   \   
+                
+            3 Intentos faltantes _X_ _X_ _X_ __ __ __""")
+
+    elif intento == 2:
+        print(""" 
+                -------
+                |      |
+                |      o
+                |     \ 
+                |       
+               / \      
+              /   \   
+                
+            4 Intentos faltantes _X_ _X_ __ __ __ __""")
+
+    elif intento == 1:
+        print(""" 
+                -------
+                |      |
+                |      o
+                |      
+                |       
+               / \      
+              /   \   
+                
+            5 Intentos faltantes _X_ __ __ __ __ __""")
+
+def main():
+    num_pc = random.randint(1,100)
+    num_player = int(input('<-- ADIVINA EL NÚMERO -->\nIngresa un número entre 1 y 100: '))
+    intento = 1
+
+    while num_pc != num_player:
+        
+        if num_player < num_pc:
+            print('Ingresa un número MAYOR')
+        else:
+            print('Ingresa un número MENOR')
+        ahorcado(intento)
+        num_player = int(input('Ingresa un número: '))
+        intento += 1
+        
+    print(f'Felicitaciones adivinaste el número, usaste {intento} intentos')
+
+if __name__ == "__main__":
+    main()
